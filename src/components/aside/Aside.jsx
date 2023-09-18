abb412f1a5985dedcdb0e449f55667d77dbdcb0e
Maternykh <maternyh.egor@yandex.ru>
2023-09-18 20:43:40 +0500

    first

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -3,10 +3,9 @@ const Aside = () => {
   return (
     <div>
      <div className='nav'>
+      <div className="catalog"><a href='#presentation'>Главная</a></div>
       <div className="catalog"><a href='#items'>Каталог</a></div>
-      <div className="catalog">Оплата</div>
-      <div className="catalog">Доставка</div>
-      <div className="catalog">Контакты</div>
+      <div className="catalog">Блог</div>
      </div>
     </div>
   )
--- src/components/main/Presentation.jsx
+++ src/components/main/Presentation.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Presentation extends Component {
   render() {
     return (
-      <div className='presentation'>
+      <div className='presentation' id='presentation'>
       <h1>Органическая косметика</h1>
       <p className='text'>Наш интернет-магазин предлагает широкий ассортимент органической косметики c сертификатом качества и проверенными составами. Желаем удачных покупок.</p>
       <div className="img"></div>
