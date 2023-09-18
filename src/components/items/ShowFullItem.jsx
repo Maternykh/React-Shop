09b730f4763f38141f53cc065de80c2c8115508e
Maternykh <maternyh.egor@yandex.ru>
2023-09-18 20:35:53 +0500

    first commit

--- src/components/items/ShowFullItem.jsx
+++ src/components/items/ShowFullItem.jsx
@@ -3,14 +3,15 @@ export class ShowFullItem extends Component {
   render() {
     return (
       <div className='full-item'>
-        <div className='full-info'>
-            <img src={this.props.item.image} className='full-image'/>
-            <div className="full-desc">{this.props.item.desc}</div>
+        <div className="full-con">
+          <img className='full-image' src={this.props.item.image} />
+          <div className="full-cont-title">
+            <div className='full-title'>{this.props.item.title}</div>
+            <div className='full-title'>{this.props.item.price} руб</div>
+          </div>          
         </div>
-      <div className='full-title'>{this.props.item.title}</div>
-      <div className='full-title'>{this.props.item.price} руб</div>
-      <div className="full-contin"><div className="full" onClick={() => this.props.onShowItem(this.props.item)}>назад</div>
-      </div>
+        <div className="full-desc">{this.props.item.desc}</div>  
+        <div className="full-contin"><div className="full" onClick={() => this.props.onShowItem(this.props.item)}>назад</div></div>
       </div>
     )
   }
