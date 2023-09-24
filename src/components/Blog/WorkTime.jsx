a2d4f88baa2c8fd407a4d374e7326a3e17e7cd34
Maternykh <maternyh.egor@yandex.ru>
2023-09-24 19:35:14 +0500

    first commit

--- src/components/Blog/WorkTime.jsx
+++ src/components/Blog/WorkTime.jsx
@@ -5,6 +5,7 @@ const WorkTime = () => {
     <div className='WorkTime'>
       <div className="worktitle">Время работы</div>
       <div className="worktime">Пн-Вс с 9:00 до 21:00</div>
+      <div className="worktime">Created by Maternykh Egor</div>
     </div>
   )
 }
